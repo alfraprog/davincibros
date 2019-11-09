7439cd26a0a0e78804332ff9e5fc86bfa83cb538
Remi Deltombe <deltombe.remi@gmail.com>
2019-11-09 12:11:42 +0100

    Basic manuscrits structure creation

---
+++ Assets/Scripts/Manuscrit/Factory/BookFactory.cs
@@ -0,0 +1,10 @@
+
+using System;
+
+public class BookFactory
+{
+	public Book fromLibrary(Library library)
+	{
+		throw new Exception("Not implemented yet");
+	}
+}
---
+++ Assets/Scripts/Manuscrit/Factory/LibraryFactory.cs
@@ -0,0 +1,15 @@
+
+using System;
+
+public class LibraryFactory
+{
+	public Library fromJSON(string path)
+	{
+		throw new Exception("Not implemented yet");
+	}
+
+	public Library fromSerializable(LibrarySerializable serializable)
+	{
+		throw new Exception("Not implemented yet");
+	}
+}
