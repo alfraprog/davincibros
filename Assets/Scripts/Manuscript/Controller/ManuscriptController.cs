0826e842397f72134666edf1c7c09d9c24c1e3df
Remi Deltombe <deltombe.remi@gmail.com>
2019-11-09 17:44:10 +0100

    dynamic image loading

--- Assets/Scripts/Manuscript/Controller/ManuscriptController.cs
+++ Assets/Scripts/Manuscript/Controller/ManuscriptController.cs
@@ -14,6 +14,10 @@ public class ManuscriptController : MonoBehaviour
         title.GetComponent<UnityEngine.UI.Text>().text = manuscript.title;
         description.GetComponent<UnityEngine.UI.Text>().text = manuscript.description;
 
+		Texture2D imageTexture = Resources.Load<Texture2D>(manuscript.imagePath);
+        Sprite imageSprite = Sprite.Create(imageTexture, new Rect(0.0f, 0.0f, imageTexture.width, imageTexture.height), new Vector2(0.5f, 0.5f), 100.0f);
+		image.GetComponent<UnityEngine.UI.Image>().sprite  = imageSprite;
+
 		Texture2D backgroundImageTexture = Resources.Load<Texture2D>(manuscript.backgroundPath);
         Sprite backgroundImageSprite = Sprite.Create(backgroundImageTexture, new Rect(0.0f, 0.0f, backgroundImageTexture.width, backgroundImageTexture.height), new Vector2(0.5f, 0.5f), 100.0f);
 		background.GetComponent<UnityEngine.UI.Image>().sprite  = backgroundImageSprite;
