5272f536015bfc496272260556793f1b92e76b63
Gogs <gogs@fake.local>
2019-11-10 14:33:16 +0100

    Add Effects cannonlaunch and explosion

---
+++ Assets/Effects/EffectCollection.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EffectCollection : MonoBehaviour
+{
+    public GameObject cannonLaunchEffectPrefab;
+
+    public GameObject explosionPrefab;
+
+    public GameObject GetLaunchSmokeEffect() 
+    { 
+        return Instantiate(cannonLaunchEffectPrefab, new Vector3(0, 0, 0), Quaternion.identity);
+    }
+
+    public GameObject GetExplosionEffect()
+    {
+        return Instantiate(cannonLaunchEffectPrefab, new Vector3(0, 0, 0), Quaternion.identity);
+    }
+
+}
