3209d859017b507035d679b70e653dfdb95b4a6f
Patrick Keller <kelpa197@hotmail.com>
2019-11-10 15:27:25 +0100

    add main scene

---
+++ Assets/Scripts/GameLoader.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameLoader : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameManager.Instance.StartGame();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
